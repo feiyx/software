2188eec80c8ba9b6ebd37be465f54ba9976291a1
feiyx <v_feiyuanxing@baidu.com>
2017-12-22 14:13:22 +0800

    有的同学反馈说自启动有失败的情况，升级为用root权限安装

--- op/yj_install_sendIP_raspberry.sh
+++ op/yj_install_sendIP_raspberry.sh
@@ -5,6 +5,12 @@
 #@TARGET: auto send ip by shell
 #@CopyRight:本脚本开源Github地址（https://github.com/feiyx）
 
+#切换到root用户
+WHOAMI=`whoami`
+if [ ! ${WHOAMI} = "root" ] ; then
+	echo "请输入本机root用户密码，如果忘记请在pi用户下执行（sudo passwd root）："
+	su root
+fi
 
 read -p "请输入你的126邮箱：" yourMail
 read -p "请输入你的126邮箱密码，切记此处是授权码，获得途径请查看：https://jingyan.baidu.com/article/9faa72318b76bf473c28cbf7.html
@@ -17,7 +23,7 @@ mkdir -p ~/script && cd ~/script/
 ROOT_HOME=$(cd `dirname $0`; pwd)
 
 install_soft(){
-	echo "开始安装msmtp and mutt ..."
+	echo "开始安装基础环境(msmtp and mutt ...)"
 	sudo apt install msmtp
 	sudo apt install mutt
 
@@ -48,43 +54,71 @@ chmod 0600 ~/.msmtprc
 
 
 sudo echo '#!/bin/bash
-#@author：feiyuanxing
-#@date：2017-12-20
-#@E-Mail：feiyuanxing@gmail.com
-#@TARGET: auto send ip by shell
-#@CopyRight:本脚本开源Github地址（https://github.com/feiyx）
 
-# check network availability
-while true
-do
-  TIMEOUT=5
-  SITE_TO_CHECK="www.baidu.com"
-  RET_CODE=`curl -I -s --connect-timeout $TIMEOUT $SITE_TO_CHECK -w %{http_code} | tail -n1`
-  if [ "x$RET_CODE" = "x200" ]; then
-  echo "Network OK, will send mail..."
-  break
-  else
-  echo "Network not ready, wait..."
-  sleep 1s
-  fi
-done
+### BEGIN INIT INFO  
+# Provides:          autoSendIp 
+# Required-Start:    $remote_fs
+# Required-Stop:     $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# author：feiyuanxing
+# E-Mail：feiyuanxing@gmail.com
+# date：2017-12-20
+# TARGET: auto send ip by shell
+# @CopyRight:本脚本开源Github地址（https://github.com/feiyx）
+# Short-Description: Start or stop the /dev/video0 
+### END INIT INFO
 
-#ETH0_IP_ADDR=`ifconfig eth0 | sed -n "2,2p" | awk "{print substr($2,1)}"`
-ETH0_IP_ADDR=`ifconfig `
-# send the Email
-echo "Current time: `date "+%F %T"`. Enjoy it ${ETH0_IP_ADDR}" | mutt -s "IP Address of Raspberry Mail" '${yourMail}'
+start(){
+	su pi
+	# check network availability
+	while true
+	do
+	  TIMEOUT=5
+	  SITE_TO_CHECK="www.baidu.com"
+	  RET_CODE=`curl -I -s --connect-timeout $TIMEOUT $SITE_TO_CHECK -w %{http_code} | tail -n1`
+	  if [ "x$RET_CODE" = "x200" ]; then
+	  echo "Network OK, will send mail..."
+	  break
+	  else
+	  echo "Network not ready, wait..."
+	  sleep 1s
+	  fi
+	done
 
-' > auto_send_ip.sh
-sudo sed -i '/auto_send_ip/d' /etc/rc.local
-#sudo echo "${ROOT_HOME}/auto_send_ip.sh" >> /etc/rc.local
-#sudo sh -c ''${ROOT_HOME}'/auto_send_ip.sh >> /etc/rc.local' 
-sudo sh -c 'echo "su - pi -c \"/bin/sh '${ROOT_HOME}'/auto_send_ip.sh\"" >> /etc/rc.local' 
+	#ETH0_IP_ADDR=`ifconfig eth0 | sed -n "2,2p" | awk "{print substr($2,1)}"`
+	ETH0_IP_ADDR=`ifconfig `
+	# send the Email
+	echo "Current time: `date "+%F %T"`. Enjoy it ${ETH0_IP_ADDR}" | mutt -s "IP Address of Raspberry Mail" '${yourMail}'
+
+}
+
+stop(){
+    echo -n "stop osprey"
+}
+
+case $1 in
+start)
+    start
+    ;;
+stop)
+    stop
+    ;;
+*)
+    echo "Usage: $0 (start|stop)"
+    ;;
+esac
+
+
+' >  /etc/init.d/autoSendIp
+chmod +x /etc/init.d/autoSendIp
+update-rc.d autoSendIp defaults
 
 echo "安装成功，试试重启自动发送邮件到${yourMail}!"
 
 
 }
 
-install_soft
+#install_soft
 configure_env
 
